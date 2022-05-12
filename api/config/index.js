39c342583d517f4b1a3345da07bd46c8662944b4
Evgeniy Galkovskiy <zhegal@gmail.com>
2022-05-12 22:09:34 +0300

    006. creating database

---
+++ api/config/index.js
@@ -0,0 +1,3 @@
+module.exports.port = process.env.PORT || 3888;
+module.exports.host = process.env.HOST;
+module.exports.db = process.env.MONGO_URL;
--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const { connectDb } = require('../helpers/db');
+const { host, port } = require('../config');
 const app = express();
-const port = process.env.PORT || 3888;
-const host = process.env.HOST;
+
+const startServer = () => {
+    app.listen(port, () => {
+        console.log(`API service is started on ${port} port`);
+        console.log(`On host ${host}`);
+    });
+}
 
 app.get('/test', (req, res) => {
     res.send('API server is working correct!');
 });
 
-app.listen(port, () => {
-    console.log(`API service is started on ${port} port`);
-    console.log(`On host ${host}`);
-});
+connectDb()
+    .on('error', console.log)
+    .on('disconnect', connectDb)
+    .once('open', startServer);
