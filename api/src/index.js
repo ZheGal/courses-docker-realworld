9317434f61a46219b9f2f84f8586058e944b2615
Evgeniy Galkovskiy <zhegal@gmail.com>
2022-05-12 21:54:13 +0300

    005. env configs

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3888;
+const host = process.env.HOST;
 
 app.get('/test', (req, res) => {
     res.send('API server is working correct!');
 });
 
-app.listen(3000, () => {
-    console.log('API service is started on 3000 port');
+app.listen(port, () => {
+    console.log(`API service is started on ${port} port`);
+    console.log(`On host ${host}`);
 });
